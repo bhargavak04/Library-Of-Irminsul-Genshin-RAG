87ef5763411bdf05731ef20f6944227886d6e55e
Bhargav <originaltrigger3@gmail.com>
2025-04-20 17:49:39 +0530

    render port

--- app.py
+++ app.py
@@ -231,4 +231,4 @@ async def health_check():
 # ------------------------------------------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
